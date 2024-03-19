bcd0b4eb2fb58e9328d26beb453531a5dcfb27fd
adamd <adam.d.hinton@gmail.com>
2024-03-19 14:08:16 -0400

    flesh out Task.java

---
+++ src/dev/lpa/Task.java
@@ -0,0 +1,14 @@
+package dev.lpa;
+
+enum Priority{HIGH, MDEIUM, LOW}
+
+eum Status {IN_QUEUE, ASSIGNED, IN_PROGRESS}
+
+public class Task {
+
+    // Each task belongs to some project
+    private String project;
+    private String description;
+    private String assignee;
+
+}
