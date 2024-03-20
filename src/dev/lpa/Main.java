aa639e629a9cfd43051153cfc44d3216a222e14c
adamd <adam.d.hinton@gmail.com>
2024-03-19 21:54:29 -0400

    solution to first question

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -28,4 +28,41 @@ public class Main {
         list.sort(sorter);
         list.forEach(System.out::println);
     }
+
+    public static Set<Task> getUnion(Set<Task> setA, Set<Task> setB){
+        Set<Task> unionSet = new HashSet<>(setA);
+        unionSet.addAll(setB);
+
+        return unionSet;
+    }
+
+    public static Set<Task> getIntersect(Set<Task> setA, Set<Task> setB){
+        Set<Task> intersectionSet = new HashSet<>();
+
+
+
+        return intersectionSet;
+    }
+
 }
+
+// I'm a new manager of a team of three : Ann, Bob, Carol
+// Each has a set of tasks. No good way to know who's working on what
+// I've asked them to submit to me what they're owrking on
+// I also have a master set of tasks which my boss sent to me
+
+// Questions:
+// What's the full task list?
+    // The full list of all tasks from mgr, and any additional tasks the EEs have that aren't on the list
+// Which tasks belong to at least one team member?
+// Which tasks still need to be assigned?
+// Which tasks are assigned to multiple EEs?
+
+//Create three methods on main
+    // DONT MUTATE PARAMETERS. ***RETURN NEW SET***
+// Main.getUnion
+    // takes List of Sets, returns union of all the sets
+// Main.getIntersect
+    // takes two Sets, returns intersection of the sets
+// Main.getDifference
+    // takes two Sets, removes second arg's sets from the first
